156def0c1e38fe3249da0361fe2290849a1d2e5c
igorashs <igorashsirbu@gmail.com>
2020-07-03 10:34:22 +0300

    add fetch methods

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,8 +2,30 @@
 
 const API_URL = 'http://localhost:9000/';
 
+export async function fetchServerStatus() {
+  const data = await fetch(API_URL);
+
+  return await data.json();
+}
+
 export async function fetchPublishedPosts() {
   const data = await fetch(`${API_URL}posts/published/`);
 
   return await data.json();
+}
+
+export async function fetchPostWithId(id) {
+  const data = await fetch(`${API_URL}/posts/${id}`);
+
+  return await data.json();
+}
+
+export async function fetchPostCommentsWithId(id) {
+  const data = await fetch(`${API_URL}/posts/${id}/comments`);
+
+  return await data.json();
+}
+
+export async function postPostCommentWithId(id, data) {
+  // TODO
 }
