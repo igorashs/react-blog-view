005eafb9ab2430ef18c47cfee043aa61948b7d24
igorashs <igorashsirbu@gmail.com>
2020-07-06 11:41:48 +0300

    add initial markup

--- src/components/PostPage/PostPage.js
+++ src/components/PostPage/PostPage.js
@@ -1,5 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { fetchPostCommentsWithId } from '../../lib/api';
+import moment from 'moment';
 
-export default function PostPage() {
-  return <h1>Post Page</h1>;
+export default function PostPage({ posts }) {
+  const { postId } = useParams();
+  const history = useHistory();
+  const [comments, setComments] = useState([]);
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    const post = posts && posts.find((p) => p._id === postId);
+    if (post) {
+      setPost(post);
+    } else {
+      history.push('/');
+    }
+  }, [postId, posts, history]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      if (post) {
+        const comments = await fetchPostCommentsWithId(postId);
+
+        if (!comments.error) {
+          addTimestamps(comments);
+        }
+
+        setComments(comments || []);
+      }
+    };
+    fetch();
+  }, [postId, post]);
+
+  return !post ? null : (
+    <article>
+      <header>
+        <h1>
+          New: <span className="Italic">{post.title}</span>
+        </h1>
+      </header>
+      <section>
+        <p>{post.text}</p>
+      </section>
+      <footer>
+        <p>Date: {post.timestamp}</p>
+        <section>
+          <h2>Comments</h2>
+          <ul>
+            {comments.map((c) => (
+              <li key={c._id}>
+                <article>
+                  <h3>{c.username}</h3>
+                  <div>
+                    <p>{c.text}</p>
+                    <p>{c.timestamp}</p>
+                  </div>
+                </article>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </footer>
+    </article>
+  );
+}
+
+PostPage.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.object)
+};
+
+// add timestamps to state array (which have date prop)
+// doesn't modify the state if it has an invalid type
+function addTimestamps(dataArr) {
+  if (dataArr instanceof Array) {
+    dataArr.forEach((data) => {
+      if (data.date) {
+        return (data.timestamp = moment(data.date).calendar());
+      }
+    });
+  }
 }
--- src/components/PostsPage/PostsPage.js
+++ src/components/PostsPage/PostsPage.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export default function PostsPage() {
-  return <h1>Posts Page</h1>;
+export default function PostsPage({ posts }) {
+  return !posts ? (
+    <h3>No Data Found :C</h3>
+  ) : (
+    <ul>
+      {posts.map((post) => (
+        <li key={post._id}>
+          <Link to={`/posts/${post._id}`}>
+            <article>
+              <h3>{post.title}</h3>
+              <p>{post.timestamp}</p>
+            </article>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
+
+PostsPage.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.object)
+};
