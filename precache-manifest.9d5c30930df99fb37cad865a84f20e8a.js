2fd03ed21b134692ac52d15f120d9b33eaf83e00
igorashs <igorashsirbu@gmail.com>
2020-09-10 13:48:29 +0300

    build

---
+++ precache-manifest.9d5c30930df99fb37cad865a84f20e8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a75dcaa0ab09d05ed40a2b09b93a0783",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecb8f7cdb75bb83c230b",
+    "url": "/static/css/main.1e867e88.chunk.css"
+  },
+  {
+    "revision": "376dfc28f58e2df4712b",
+    "url": "/static/js/2.f3f3278d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f3f3278d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb8f7cdb75bb83c230b",
+    "url": "/static/js/main.6141bf4b.chunk.js"
+  },
+  {
+    "revision": "c1679548cec1aac9c660",
+    "url": "/static/js/runtime-main.8da568a6.js"
+  }
+]);
