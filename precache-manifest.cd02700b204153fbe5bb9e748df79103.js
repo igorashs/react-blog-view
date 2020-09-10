c925a35c394bb0c3c6723d37e5e5c96ed9ef92df
igorashs <igorashsirbu@gmail.com>
2020-09-10 18:01:10 +0300

    update build

---
+++ precache-manifest.cd02700b204153fbe5bb9e748df79103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783891572edc3335882db5080d246fd0",
+    "url": "/react-blog-view/index.html"
+  },
+  {
+    "revision": "87a6714ad92460e62683",
+    "url": "/react-blog-view/static/css/main.1e867e88.chunk.css"
+  },
+  {
+    "revision": "376dfc28f58e2df4712b",
+    "url": "/react-blog-view/static/js/2.f3f3278d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-blog-view/static/js/2.f3f3278d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87a6714ad92460e62683",
+    "url": "/react-blog-view/static/js/main.fea70c02.chunk.js"
+  },
+  {
+    "revision": "e3a0f17b64fee3c39c56",
+    "url": "/react-blog-view/static/js/runtime-main.27171349.js"
+  }
+]);
